d5f40faddf77c950fd8c90875ac35124da0a2515
Rahul <rahuls.shetty01@gmail.com>
2026-09-03 00:53:32 +0530

    added firmware configuration template

---
+++ firmware/main/configuration.example.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define WIFI_SSID "YOUR_WIFI_NAME"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+#define MQTT_BROKER "YOUR_EMQX_HOST"
+#define MQTT_PORT 8883
+
+#define MQTT_USERNAME "YOUR_MQTT_USERNAME"
+#define MQTT_PASSWORD "YOUR_MQTT_PASSWORD"
+
+#define CLIENT_ID "AMBULANCE001"
+
+#define TOPIC_VITALS "smartambulance/ambulance001/vitals"
+#define TOPIC_GPS "smartambulance/ambulance001/gps"
+#define TOPIC_ECG "smartambulance/ambulance001/ecg"
+#define TOPIC_TRAFFIC "smartambulance/ambulance001/traffic"
+#define TOPIC_ALERTS "smartambulance/ambulance001/alerts"
+#define TOPIC_PREDICTION "smartambulance/ambulance001/prediction"
